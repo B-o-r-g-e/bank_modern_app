4bfcc97336a5294df5202197ce8a99c0db926440
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-20 08:46:22 +0100

    .

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -2,7 +2,14 @@ import styles from './style.js'
 
 const App = () => (
     <div className="bg-primary w-full overflow-hidden">
-        <div className={`${styles.paddingX}`}></div>
+        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+                Navbar
+            </div>
+        </div>
+        <div className={`bg-primary ${styles.flexStart}}`}>
+
+        </div>
     </div>
 )
 
