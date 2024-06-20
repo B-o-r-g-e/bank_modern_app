0dee85f484a3d2d041001384fb690f4ee8b4935b
babst3alth <babalolaoluwapelumi9@gmail.com>
2024-06-20 11:00:22 +0100

    .

--- bank_modern_app/src/components/Navbar.jsx
+++ bank_modern_app/src/components/Navbar.jsx
@@ -27,7 +27,9 @@ const Navbar = () => {
                 ))}
             </ul>
 
-
+            <div className={"sm:hidden justify-end items-center"}>
+                <img src="" alt=""/>
+            </div>
 
         </nav>
     )
